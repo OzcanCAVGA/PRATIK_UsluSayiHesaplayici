a33f77c3ed1d4e58d36fb2df090582cb0ec017e5
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 19:58:38 +0300

    Create Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+/*
+Ödev
+Java ile kullanıcının girdiği değerler ile üslü sayı hesaplayan programı "For Döngüsü" kullanarak yapınız.
+ */
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner yazici = new Scanner(System.in);
+        System.out.println("Taban degerini giriniz: ");
+        int taban,us,deger=1;
+        taban=yazici.nextInt();
+
+        System.out.println("Us degerini giriniz: ");
+        us=yazici.nextInt();
+
+        for (int i=1;i<=us;i++){
+            deger=deger*taban;
+        }
+        System.out.println(deger);
+
     }
 }
